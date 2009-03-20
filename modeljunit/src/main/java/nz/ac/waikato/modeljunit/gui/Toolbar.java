920c26c95d6ff24a4672fb0a353c249ce11f2602
gianp <gianp@ef480773-d1e6-4e20-a450-f5b107aebd75>
2009-03-20 00:10:05 +0000

    Updated the GUI to use the visualiser panel correctly, and added a new toolbar.

---
+++ modeljunit/src/main/java/nz/ac/waikato/modeljunit/gui/Toolbar.java
@@ -0,0 +1,78 @@
+/**
+Copyright (C) 2009 ModelJUnit Project
+This file is part of the ModelJUnit project.
+
+The ModelJUnit project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The ModelJUnit project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ModelJUnit; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package nz.ac.waikato.modeljunit.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/** The toolbar class for ModelJUnit.
+ *
+ * Holds a reference to the parent window so as to enable button actions. 
+ *
+ * @author Gian Perrone <gian@waikato.ac.nz>
+ **/
+public class Toolbar extends JPanel
+{
+   private ModelJUnitGUI mParent;
+
+   public Toolbar(ModelJUnitGUI parent) {
+      super();
+      mParent = parent;
+      this.setLayout(new FlowLayout(FlowLayout.LEFT));
+      buildGUI();
+   }
+
+   public void buildGUI() {
+      JButton b = new JButton("New",createImageIcon("/images/New16.gif", "New Project"));
+      this.add(b);
+      
+      b = new JButton("Open", createImageIcon("/images/Open16.gif", "Open Project"));
+      this.add(b);
+
+      b = new JButton("Save", createImageIcon("/images/Save16.gif", "Save Project"));
+      this.add(b);
+
+      b = new JButton("Results",createImageIcon("/images/Results16.gif", "View Results") );
+      this.add(b);
+
+      b = new JButton("Animate", createImageIcon("/images/Animate16.gif", "Animate Model"));
+      this.add(b);
+      
+      String[] configurations = {"Configuration 1", "Configuration 2", "Configuration 3"};
+      JComboBox combo = new JComboBox(configurations);
+
+      this.add(combo);
+      this.add(new JButton("Run", createImageIcon("/images/Play16.gif", "Run Configuration")));
+   }
+
+   /** Returns an ImageIcon, or null if the path was invalid. */
+   protected ImageIcon createImageIcon(String path,
+                                           String description) {
+      java.net.URL imgURL = getClass().getResource(path);
+      System.out.println("imgURL: "+imgURL);
+      if (imgURL != null) {
+          return new ImageIcon(imgURL, description);
+      } else {
+          System.err.println("Couldn't find file: " + path);
+          return null;
+      }
+   }
+
+}
