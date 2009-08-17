bbec1b115409087a0843fbb33b640f726bbca558
marku <marku@ef480773-d1e6-4e20-a450-f5b107aebd75>
2009-08-17 08:00:20 +0000

    Cleanup and renaming to make the GSM example easier to read

--- trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/examples/gsm/GSM11ImplTest.java
+++ trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/examples/gsm/GSM11ImplTest.java
@@ -1,10 +1,9 @@
 package nz.ac.waikato.modeljunit.examples.gsm;
 
+import nz.ac.waikato.modeljunit.GraphListener;
 import nz.ac.waikato.modeljunit.GreedyTester;
 import nz.ac.waikato.modeljunit.RandomTester;
-import nz.ac.waikato.modeljunit.Tester;
-import nz.ac.waikato.modeljunit.VerboseListener;
-import nz.ac.waikato.modeljunit.coverage.CoverageMetric;
+import nz.ac.waikato.modeljunit.coverage.StateCoverage;
 import nz.ac.waikato.modeljunit.coverage.TransitionCoverage;
 import junit.framework.TestCase;
 
@@ -19,14 +18,17 @@ public class GSM11ImplTest extends TestCase
   public void testGSM11()
   {
     SimCard model = new SimCard(new SimCardAdaptor());
-    RandomTester tester = new RandomTester(model);
+    RandomTester tester = new GreedyTester(model); //new RandomTester(model);
     tester.setResetProbability(0.01);
-    //tester.buildGraph(1000000);
+    GraphListener graph = tester.buildGraph(100);
     //tester.addListener("Verbose", new VerboseListener(tester.getModel()));
-    CoverageMetric trans = new TransitionCoverage();
-    tester.addListener(trans);
-    tester.generate(10000);
-    System.out.println("Transition coverage = "+trans.toString());
+    tester.addCoverageMetric(new TransitionCoverage());
+    tester.addCoverageMetric(new StateCoverage());
+    for (int m = 1; m < 100; m++) {
+    	tester.generate(1 * 100 * 1000);
+    	System.out.println("\n" + (m * 100) + "K todo=" + graph.numTodo());
+        tester.printCoverage();
+    }
   }
 
   /** In case we want to run this JUnit test from the command line. */
