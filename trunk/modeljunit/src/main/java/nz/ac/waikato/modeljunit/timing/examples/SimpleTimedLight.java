b92fa22bdfe015386a905c09f0236bff66242e16
marku <marku@ef480773-d1e6-4e20-a450-f5b107aebd75>
2009-08-29 09:56:31 +0000

    Some simplest-possible realtime models: a light with just two states and one timeout

---
+++ trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/timing/examples/SimpleTimedLight.java
@@ -0,0 +1,67 @@
+package nz.ac.waikato.modeljunit.timing.examples;
+
+import java.util.Random;
+
+import nz.ac.waikato.modeljunit.Action;
+import nz.ac.waikato.modeljunit.RandomTester;
+import nz.ac.waikato.modeljunit.Tester;
+import nz.ac.waikato.modeljunit.VerboseListener;
+import nz.ac.waikato.modeljunit.timing.Time;
+import nz.ac.waikato.modeljunit.timing.TimedFsmModel;
+import nz.ac.waikato.modeljunit.timing.Timeout;
+
+/**
+ * This models a simple light switch that turns off automatically
+ * after 60 seconds.
+ * 
+ * The model is written using the real-time features of ModelJUnit,
+ * and illustrates the recommended way of specifying a simple real-time
+ * system.
+ * 
+ * @author marku
+ */
+public class SimpleTimedLight implements TimedFsmModel
+{
+  private Random ran = new Random(12345);
+  private boolean on = false;
+  @Time public int time = 0;
+  @Timeout("autoOff") public int offTimeout = -1;
+
+  @Override
+  public Object getState() {
+    return on ? "On" : "Off"; //new TimedState(on ? "On" : "Off", time);
+  }
+
+  @Override public void reset(boolean testing) {
+    on = false;
+    time = 0;
+    offTimeout = -1;
+  }
+
+  @Action public void pushButton() {
+    on = true;  // turn the light on
+    offTimeout = time + 60; // enable the timeout
+  }
+
+  public boolean autoOffGuard() { return time == offTimeout; }
+  @Action public void autoOff() {
+    on = false;
+    offTimeout = -1;
+  }
+
+  @Override
+  public int getNextTimeIncrement()
+  {
+    return ran.nextInt(300);
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    Tester tester = new RandomTester(new SimpleTimedLight());
+    tester.addListener(new VerboseListener());
+    tester.generate(200);
+  }
+}
