6b641d316d798d10f94070e7a990943774abf474
marku <marku@ef480773-d1e6-4e20-a450-f5b107aebd75>
2009-09-30 19:49:15 +0000

    Experiments for the ModelJUnit tutorial chapter.

--- trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/timing/examples/SimpleNaiveLight.java
+++ trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/timing/examples/SimpleNaiveLight.java
@@ -9,6 +9,8 @@ import nz.ac.waikato.modeljunit.GraphListener;
 import nz.ac.waikato.modeljunit.RandomTester;
 import nz.ac.waikato.modeljunit.Tester;
 import nz.ac.waikato.modeljunit.VerboseListener;
+import nz.ac.waikato.modeljunit.coverage.CoverageMetric;
+import nz.ac.waikato.modeljunit.coverage.TransitionCoverage;
 
 
 /**
@@ -59,7 +61,7 @@ public class SimpleNaiveLight implements FsmModel
 
   public boolean delayGuard() { return time != offTimeout; }
   @Action public void delay() {
-    time += 1 + ran.nextInt(200);
+    time += 1 + ran.nextInt(60);
     //System.out.println("     time := " + time + " but offTimeout=" + offTimeout);
     if (offTimeout >= 0 && time > offTimeout) {
       time = offTimeout; // don't go past the timeout
@@ -72,10 +74,46 @@ public class SimpleNaiveLight implements FsmModel
    * @throws FileNotFoundException 
    */
   public static void main(String[] args) throws FileNotFoundException {
-    Tester tester = new RandomTester(new SimpleNaiveLight());
-    tester.addListener(new VerboseListener());
+    final int experiments = 100;
+    SimpleNaiveLight fsm = new SimpleNaiveLight();
+    Tester tester = new RandomTester(fsm);
+    //tester.addListener(new VerboseListener());
+    //tester.addListener(new VerboseListener());
     GraphListener graph = tester.buildGraph();
-    tester.generate(50);
-    graph.printGraphDot("SimpleNaiveLight.dot");
+    System.out.println("FSM has " + graph.getGraph().numVertices() + " states and "
+        + graph.getGraph().numEdges() + " transitions");
+    CoverageMetric metric = tester.addCoverageMetric(new TransitionCoverage());
+    for (double prob = 0.1; prob < 0.99; prob += 0.1) {
+      double totalSteps = 0.0;
+      int totalTime = 0; // sum of all the totalTimes.
+      for (int seed = 0; seed < experiments; seed++) {
+        int testSeqTime = 0;
+        int lastTime = 0; // the time after the last transition
+        metric.clear();
+        tester.reset();
+        tester.setRandom(new Random(seed));
+        int count = 0;
+        while (metric.getPercentage() < 100.0) {
+          tester.generate(1);
+          if (fsm.time > lastTime) {
+            lastTime = fsm.time;
+          } else {
+            testSeqTime += lastTime;
+            //System.out.println("added " + lastTime + " secs");
+            lastTime = 0;
+          }
+          count++;
+        }
+        testSeqTime += lastTime;
+        //System.out.println("finally added " + lastTime + " secs");
+        totalSteps += count;
+        totalTime += testSeqTime;
+        //System.out.println("Seed=" + seed + " steps=" + count + " time=" + testSeqTime);
+      }
+      System.out.println(String.format("%.2f", prob) + "," + totalSteps / experiments
+          + "," + (double) totalTime / experiments);
+    }
+    System.out.println("FSM has " + graph.getGraph().numVertices() + " states and "
+        + graph.getGraph().numEdges() + " transitions");
   }
 }
--- trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/timing/examples/SimpleTimedLight.java
+++ trunk/modeljunit/src/main/java/nz/ac/waikato/modeljunit/timing/examples/SimpleTimedLight.java
@@ -1,13 +1,18 @@
 package nz.ac.waikato.modeljunit.timing.examples;
 
+import java.io.FileNotFoundException;
 import java.util.Random;
 
 import nz.ac.waikato.modeljunit.Action;
+import nz.ac.waikato.modeljunit.GraphListener;
 import nz.ac.waikato.modeljunit.RandomTester;
 import nz.ac.waikato.modeljunit.Tester;
 import nz.ac.waikato.modeljunit.VerboseListener;
+import nz.ac.waikato.modeljunit.coverage.CoverageMetric;
+import nz.ac.waikato.modeljunit.coverage.TransitionCoverage;
 import nz.ac.waikato.modeljunit.timing.Time;
 import nz.ac.waikato.modeljunit.timing.TimedFsmModel;
+import nz.ac.waikato.modeljunit.timing.TimedModel;
 import nz.ac.waikato.modeljunit.timing.Timeout;
 
 /**
@@ -47,17 +52,60 @@ public class SimpleTimedLight implements TimedFsmModel
 
   @Override
   public int getNextTimeIncrement(Random ran) {
-    return 1 + ran.nextInt(200);
+    return 1 + ran.nextInt(60);
   }
 
   /**
    * An example of generating tests from the above model.
    * @param args
+   * @throws FileNotFoundException 
    */
-  public static void main(String[] args)
+  public static void main(String[] args) throws FileNotFoundException
   {
-    Tester tester = new RandomTester(new SimpleTimedLight());
-    tester.addListener(new VerboseListener());
-    tester.generate(20);
+    final int experiments = 100;
+    TimedModel model = new TimedModel(new SimpleTimedLight());
+    Tester tester = new RandomTester(model);
+    //tester.addListener(new VerboseListener());
+    double origProb = model.getTimeoutProbability();
+    model.setTimeoutProbability(0.3); // while exploring the FSM
+    GraphListener graph = tester.buildGraph();
+    model.setTimeoutProbability(origProb);
+    graph.printGraphDot("SimpleTimedLight.dot");
+    System.out.println("FSM has " + graph.getGraph().numVertices() + " states and "
+        + graph.getGraph().numEdges() + " transitions");
+    CoverageMetric metric = tester.addCoverageMetric(new TransitionCoverage());
+    for (double prob = 0.1; prob < 0.99; prob += 0.1) {
+      model.setTimeoutProbability(prob);
+      double totalSteps = 0.0;
+      int totalTime = 0; // sum of all the totalTimes.
+      for (int seed = 0; seed < experiments; seed++) {
+        int testSeqTime = 0;
+        int lastTime = 0; // the time after the last transition
+        metric.clear();
+        tester.reset();
+        tester.setRandom(new Random(seed));
+        int count = 0;
+        while (metric.getPercentage() < 100.0) {
+          tester.generate(1);
+          if (model.getTime() > lastTime) {
+            lastTime = model.getTime();
+          } else {
+            testSeqTime += lastTime;
+            //System.out.println("added " + lastTime + " secs");
+            lastTime = 0;
+          }
+          count++;
+        }
+        testSeqTime += lastTime;
+        //System.out.println("finally added " + lastTime + " secs");
+        totalSteps += count;
+        totalTime += testSeqTime;
+        //System.out.println("Seed=" + seed + " steps=" + count + " time=" + testSeqTime);
+      }
+      System.out.println(String.format("%.2f", prob) + "," + totalSteps / experiments
+          + "," + (double) totalTime / experiments);
+    }
+    System.out.println("FSM has " + graph.getGraph().numVertices() + " states and "
+        + graph.getGraph().numEdges() + " transitions");
   }
 }
