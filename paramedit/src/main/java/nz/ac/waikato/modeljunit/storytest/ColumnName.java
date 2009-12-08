7bd3fee3b926d9285a8396d1ab783d5a5957eef9
simianware <simianware@ef480773-d1e6-4e20-a450-f5b107aebd75>
2009-12-08 11:15:35 +0000

    added

---
+++ paramedit/src/main/java/nz/ac/waikato/modeljunit/storytest/ColumnName.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package nz.ac.waikato.modeljunit.storytest;
+
+/**
+ * @author root
+ *
+ */
+public class ColumnName
+{
+  String mName;
+  
+  public ColumnName(String name) 
+  {
+    mName = name;
+  }
+  
+  public String toString()
+  {
+    return mName;
+  }
+
+  public String getName()
+  {
+    return mName;
+  }
+
+  public void setName(String name)
+  {
+    mName = name;
+  }
+}
